2be0159b1ffc43fb26a070ec2266e2c654d2d21c
Juan11781 <juan.m117@outlook.com>
2025-07-08 15:10:53 -0600

    sonnar change

--- auth-service/java/com/sales/AuthServiceApplication.java
+++ auth-service/java/com/sales/AuthServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class AuthServiceApplication {
+public class AuthServiceApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
